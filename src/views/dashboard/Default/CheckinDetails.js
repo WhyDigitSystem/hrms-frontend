135ccbb7d92d186f1835ca203a9ee8a848e3ee26
Mani-Ofcl <karupu@whydigit.in>
2025-03-06 18:13:39 +0530

    checkin api integration

--- src/views/dashboard/Default/CheckinDetails.js
+++ src/views/dashboard/Default/CheckinDetails.js
@@ -18,7 +18,7 @@ const CardWrapper = styled(MainCard)(({ theme }) => ({
   boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)',
   transition: 'transform 0.3s ease-in-out',
   '&:hover': {
-    transform: 'scale(1.02)',
+    transform: 'scale(1.02)'
   },
   '&:after': {
     content: '""',
@@ -28,7 +28,7 @@ const CardWrapper = styled(MainCard)(({ theme }) => ({
     background: theme.palette.secondary[800],
     borderRadius: '50%',
     top: -85,
-    right: -95,
+    right: -95
   },
   '&:before': {
     content: '""',
@@ -39,7 +39,7 @@ const CardWrapper = styled(MainCard)(({ theme }) => ({
     borderRadius: '50%',
     top: -125,
     right: -15,
-    opacity: 0.5,
+    opacity: 0.5
   }
 }));
 
@@ -63,20 +63,38 @@ const CheckinDetails = ({ isLoading }) => {
   const time = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); // e.g., 09:30 AM
 
   const [orgId, setOrgId] = useState(localStorage.getItem('orgId'));
-  const [branchId, setBranchId] = useState(localStorage.getItem('branchId'));
   const [branch, setBranch] = useState(localStorage.getItem('branch'));
-  const [empcode, setEmpCode] = useState(localStorage.getItem('empcode'));
-  // const [loginUserName, setLoginUserName] = useState(localStorage.getItem('userName'));
-  // const [loginUserName, setLoginUserName] = useState(localStorage.getItem('userName'));
-  // Handle Check-In and Check-Out button clicks
+  const [empcode, setEmpCode] = useState(localStorage.getItem('employeeCode'));
   const [isCheckedIn, setIsCheckedIn] = useState(false);
+  // const handleCheckIn = async () => {
+  //   const saveCheckIN = {
+  //     status: true,
+  //     orgId: orgId,
+  //     branch: branch,
+  //     // branchId: branchId,
+  //     empcode: empcode
+  //   };
+
+  //   try {
+  //     const result = await apiCalls('put', `basicmaster/createCheckInOut`, saveCheckIN);
+
+  //     if (result.status === true) {
+  //       showToast('success', 'Check-In Success');
+  //       setIsCheckedIn(true); // Hide Check-In button
+  //     } else {
+  //       showToast('error', result.paramObjectsMap.errorMessage || 'Check-In Failed');
+  //     }
+  //   } catch (err) {
+  //     showToast('error', 'Check-In Failed');
+  //   }
+  // };
+
   const handleCheckIn = async () => {
     const saveCheckIN = {
       status: true,
       orgId: orgId,
       branch: branch,
-      branchId: branchId,
-      empcode: empcode,
+      empcode: empcode
     };
 
     try {
@@ -84,7 +102,7 @@ const CheckinDetails = ({ isLoading }) => {
 
       if (result.status === true) {
         showToast('success', 'Check-In Success');
-        setIsCheckedIn(true); // Hide Check-In button
+        setIsCheckedIn(true); // Enable Check-Out, Disable Check-In
       } else {
         showToast('error', result.paramObjectsMap.errorMessage || 'Check-In Failed');
       }
@@ -93,14 +111,35 @@ const CheckinDetails = ({ isLoading }) => {
     }
   };
 
+  // const handleCheckOut = async () => {
+  //   const saveCheckIN = {
+  //     status: false,
+  //     orgId: orgId,
+  //     branch: branch,
+  //     // branchId: branchId,
+  //     empcode: empcode
+  //   };
+
+  //   try {
+  //     const result = await apiCalls('put', `basicmaster/createCheckInOut`, saveCheckIN);
+
+  //     if (result.status === true) {
+  //       showToast('success', 'Check-Out Success');
+  //       setIsCheckedIn(false); // Hide Check-Out, show Check-In
+  //     } else {
+  //       showToast('error', result.paramObjectsMap.errorMessage || 'Check-Out Failed');
+  //     }
+  //   } catch (err) {
+  //     showToast('error', 'Check-Out Failed');
+  //   }
+  // };
 
   const handleCheckOut = async () => {
     const saveCheckIN = {
       status: false,
       orgId: orgId,
       branch: branch,
-      branchId: branchId,
-      empcode: empcode,
+      empcode: empcode
     };
 
     try {
@@ -108,7 +147,7 @@ const CheckinDetails = ({ isLoading }) => {
 
       if (result.status === true) {
         showToast('success', 'Check-Out Success');
-        setIsCheckedIn(false); // Hide Check-Out, show Check-In
+        setIsCheckedIn(false); // Disable Check-Out, Enable Check-In
       } else {
         showToast('error', result.paramObjectsMap.errorMessage || 'Check-Out Failed');
       }
@@ -127,8 +166,8 @@ const CheckinDetails = ({ isLoading }) => {
             <Box sx={{ p: 2.25 }}>
               <Grid container direction="column">
                 {/* Profile Section */}
-                <div className='d-lg-flex justify-content-lg-between align-items-lg-center'>
-                  <div className='d-lg-flex'>
+                <div className="d-lg-flex justify-content-lg-between align-items-lg-center">
+                  <div className="d-lg-flex">
                     <Grid item>
                       <Box sx={{ display: 'flex', alignItems: 'center' }}>
                         <Avatar alt="Dineshkumar" src="/path/to/avatar.jpg" sx={{ width: 60, height: 60, mr: 2 }} />
@@ -138,15 +177,6 @@ const CheckinDetails = ({ isLoading }) => {
                         </Box>
                       </Box>
                     </Grid>
-
-                    {/* <Grid item>
-                      <Box className="d-lg-flex align-items-center ps-5 ms-4 ps-lg-5 ms-lg-0 pb-3 pb-lg-0 pt-0 pt-lg-2">
-                        <Box>
-                          <Typography sx={{ fontSize: '1rem', color: theme.palette.secondary[200] }}>{`${month} ${date}, ${year}`}</Typography>
-                          <Typography sx={{ fontSize: '1rem', color: theme.palette.secondary[200] }}>{time}</Typography> 
-                        </Box>
-                      </Box>
-                    </Grid> */}
                   </div>
                   <Grid item>
                     <Box className="d-flex justify-content-start justify-content-lg-between align-items-center">
@@ -157,13 +187,18 @@ const CheckinDetails = ({ isLoading }) => {
                         sx={{
                           mr: 2,
                           width: '120px',
+                          backgroundColor: isCheckedIn ? '#d3d3d3' : '#007bff', // Light Gray when disabled, Blue when active
+                          color: isCheckedIn ? '#6c757d' : 'white',
+                          cursor: isCheckedIn ? 'not-allowed' : 'pointer',
                           transition: 'transform 0.3s ease, box-shadow 0.3s ease',
                           '&:hover': {
-                            transform: 'scale(1.05)',
-                            boxShadow: '0px 6px 20px rgba(0, 0, 0, 0.15)',
-                          },
+                            // transform: 'scale(1.05)',
+                            // boxShadow: '0px 6px 20px rgba(0, 0, 0, 0.15)'
+                            backgroundColor: isCheckedIn ? '#d3d3d3' : '#0056b3'
+                          }
                         }}
                         onClick={handleCheckIn}
+                        disabled={isCheckedIn}
                       >
                         Check-In
                       </Button>
@@ -174,39 +209,36 @@ const CheckinDetails = ({ isLoading }) => {
                         sx={{
                           width: '120px',
                           zIndex: 1,
+                          backgroundColor: isCheckedIn ? '#007bff' : '#d3d3d3', // Red when active, Light Gray when disabled
+                          color: isCheckedIn ? 'white' : '#6c757d',
+                          cursor: isCheckedIn ? 'pointer' : 'not-allowed',
                           transition: 'transform 0.3s ease, box-shadow 0.3s ease',
                           '&:hover': {
-                            transform: 'scale(1.05)', // Slightly enlarges the button
-                            boxShadow: '0px 6px 20px rgba(0, 0, 0, 0.15)', // Adds a shadow effect on hover
-                          },
+                            backgroundColor: isCheckedIn ? '#0056b3' : '#d3d3d3',
+                            // transform: 'scale(1.05)', // Slightly enlarges the button
+                            // boxShadow: '0px 6px 20px rgba(0, 0, 0, 0.15)' // Adds a shadow effect on hover
+                          }
                         }}
                         onClick={handleCheckOut}
+                        disabled={!isCheckedIn}
                       >
                         Check-Out
                       </Button>
                     </Box>
-
                   </Grid>
-
                 </div>
 
                 {/* Recent Activity Section */}
                 <Grid item sx={{ mt: 2 }}>
-                  <Typography sx={{ fontSize: '1.2rem', fontWeight: 600, color: theme.palette.secondary[200] }}>
-                    Recent Activity
-                  </Typography>
+                  <Typography sx={{ fontSize: '1.2rem', fontWeight: 600, color: theme.palette.secondary[200] }}>Recent Activity</Typography>
                   <List>
                     <ListItem>
                       <AccessTimeIcon sx={{ mr: 2 }} color="white" />
-                      <Typography sx={{ fontSize: '1rem', color: theme.palette.secondary[200] }}>
-                        Check-In for work at 9:00 AM
-                      </Typography>
+                      <Typography sx={{ fontSize: '1rem', color: theme.palette.secondary[200] }}>Check-In for work at 9:00 AM</Typography>
                     </ListItem>
                     <ListItem>
                       <AccessTimeIcon sx={{ mr: 2 }} color="white" />
-                      <Typography sx={{ fontSize: '1rem', color: theme.palette.secondary[200] }}>
-                        Check-out for work at 12:30 PM
-                      </Typography>
+                      <Typography sx={{ fontSize: '1rem', color: theme.palette.secondary[200] }}>Check-out for work at 12:30 PM</Typography>
                     </ListItem>
                   </List>
                 </Grid>
