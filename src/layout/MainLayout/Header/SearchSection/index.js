b80a397faa46cd646e3c5316c70ade1c0610911b
DineshkumarWDS <dinesh@whydigit.in>
2025-05-12 19:20:58 +0530

    SearchSection add

--- src/layout/MainLayout/Header/SearchSection/index.js
+++ src/layout/MainLayout/Header/SearchSection/index.js
@@ -33,64 +33,60 @@ import { IconAdjustmentsHorizontal, IconSearch, IconX } from '@tabler/icons-reac
 
 const screens = [
   { name: 'Dashboard', path: '/' },
-  { name: 'Set Tax Rate', path: '/Finance/SetTaxRate' },
-  { name: 'Party Master', path: '/finance/partyMaster' },
-  { name: 'Tax Master', path: '/Finance/TaxMaster' },
-  { name: 'Taxes', path: '/Finance/Taxes' },
-  { name: 'TCS Master', path: '/Finance/tcsMaster/TcsMaster' },
-  { name: 'TDS Master', path: '/Finance/tdsMaster/TdsMaster' },
-  { name: 'HSN SAC Code', path: '/Finance/HsnSacCode' },
-  { name: 'HSN SAC Codes Listing', path: '/Finance/HsnSacCodesListing' },
-  { name: 'Group', path: '/Finance/Group' },
-  { name: 'Account', path: '/Finance/account/Account' },
-  { name: 'Exchange Rates', path: '/Finance/ExRates' },
-  { name: 'Sub Ledger Account', path: '/Finance/SubLedgerAccount' },
-  { name: 'Cost Centre', path: '/Finance/costcenter/CostCentre' },
-  { name: 'Daily Rate', path: '/Finance/daily/DailyRate' },
-  { name: 'Chart Of Costcenter', path: '/Finance/chartOfCostcenter/ChartOfCostcenter' },
-  { name: 'BRS Opening', path: '/Finance/BRSOpening' },
-  { name: 'AR Bill Balance', path: '/Finance/receiptAr/ArBillBalance' },
-  { name: 'Cheque Book Master', path: '/Finance/chequeBookMaster/ChequeBookMaster' },
-  { name: 'GL Opening Balance', path: '/Finance/glOpening/GlOpening' },
-  { name: 'Fund Transfer', path: '/Finance/FundTransfer' },
-  { name: 'General Journal', path: '/Finance/GeneralJournal/GeneralJournal' },
-  { name: 'Receipt', path: '/Finance/receipt/Receipt' },
-  { name: 'Payment', path: '/Finance/payment/Payment' },
-  { name: 'AP Bill Balance', path: '/Finance/paymentAp/ApBillBalance' },
-  { name: 'Reconcile', path: '/Finance/Reconcile/Reconcile' },
-  { name: 'Receipt Register', path: '/Finance/receiptRegister/ReceiptRegister' },
-  { name: 'Payment Register', path: '/Finance/paymentRegister/PaymentRegister' },
-  { name: 'Reconciliation Summary', path: '/Finance/ReconciliationSummaryReport/ReconciliationSummary' },
-  { name: 'Document Type Master', path: '/Finance/DocumentTypeMaster' },
-  { name: 'Document Type Mapping Master', path: '/Finance/DocumentTypeMappingMaster' },
-  { name: 'Multiple Document Id Generation Master', path: '/Finance/MultipleDocumentIdGeneration' },
-  { name: 'Reconcile Cash', path: '/Finance/ReconcileCash' },
-  { name: 'Reconcile Corp', path: '/Finance/ReconcileCorp' },
-  { name: 'Create Company', path: '/companysetup/createcompany' },
-  { name: 'Company Setup', path: '/companysetup/companysetup' },
+  // basicMaster
   { name: 'Country', path: '/basicMaster/country' },
   { name: 'State', path: '/basicMaster/state' },
   { name: 'City', path: '/basicMaster/city' },
   { name: 'Currency', path: '/basicMaster/currency' },
   { name: 'Region', path: '/basicMaster/RegionMaster' },
-  { name: 'Financial Year', path: '/basicMaster/finYear' },
-  { name: 'Roles', path: '/basicMaster/roles' },
-  { name: 'Screen Names', path: '/basicMaster/ScreenNames' },
-  { name: 'Employee', path: '/basicMaster/employee' },
-  { name: 'Charge Type Request', path: '/Finance/ChargeTypeRequest' },
-  { name: 'Tax Invoice Detail', path: '/Finance/taxInvoice/taxInvoiceDetail' },
-  { name: 'Credit Note Detail', path: '/Finance/creditNote/CreditNoteDetail' },
-  { name: 'Cost Invoice', path: '/Finance/costInvoice/CostInvoice' },
-  { name: 'Cost Debit Note', path: '/Finance/costDebitNote/CostDebitNote' },
-  { name: 'List Of Values', path: '/Finance/listOfValues/listOfValues' },
-  { name: 'Payment Voucher', path: '/Finance/paymentVoucher/paymentVoucher' },
-  { name: 'AR/AP Detail', path: '/Finance/ARAP-Details' },
-  { name: 'AR/AP Adjustment', path: '/Finance/APAP-Adjustment' },
-  { name: 'Party Master', path: '/Finance/PartyMaster' },
-  { name: 'Adjustment Journal', path: '/Finance/AdjustmentJournal' },
-  { name: 'Deposit', path: '/Finance/Deposit' },
-  { name: 'Withdrawal', path: '/Finance/Withdrawal' },
-  { name: 'Contra Voucher', path: '/Finance/ContraVoucher' }
+  { name: 'Department', path: '/basicMaster/Department' },
+  { name: 'Designation', path: '/basicMaster/Designation' },
+  { name: 'Project Master', path: '/basicMaster/ProjectMaster' },
+  { name: 'Roles And Responsibilities', path: '/basicMaster/roles' },
+
+
+  // calendar
+  { name: 'Calendar', path: '/calendar' },
+
+  // companysetup
+  { name: 'Company Setup', path: '/companysetup/companysetup' },
+  { name: 'Leave Assigned', path: '/companysetup/LeaveAssigned' },
+  { name: 'Screen Names', path: '/companysetup/ScreenNames' },
+
+  // admin
+  { name: 'User Creation', path: '/admin/user-creation/userCreation' },
+
+  // employeeMaster
+  { name: 'Employee Details', path: '/employeeMaster/employeeDetails' },
+  { name: 'Attendance Process', path: '/employeeMaster/AttendenceProcess' },
+
+  // leaveMaster
+  { name: 'Leave Type', path: '/leaveMaster/LeaveType' },
+  { name: 'Holidays', path: '/leaveMaster/Holidays' },
+
+  // salaryMaster
+  { name: 'Salary Heads', path: '/salaryMaster/salaryHeads' },
+  { name: 'Salary Structure', path: '/salaryMaster/SalaryStructure' },
+  { name: 'Salary Process', path: '/salaryMaster/SalaryProcess' },
+  { name: 'Salary Report', path: '/salaryMaster/SalaryReport' },
+
+  // me
+  { name: 'Permission Request', path: '/me/permissionRequest' },
+  { name: 'Leave Request', path: '/me/leaveRequest' },
+  { name: 'Holiday Report', path: '/me/HolidayReport' },
+  { name: 'Check In & Out', path: '/me/SwipeInSwipeOut' },
+  { name: 'Time Sheet', path: '/me/TimeSheet' },
+  { name: 'Compo Off', path: '/me/CompoOff' },
+
+  // finance
+  { name: 'Payslip', path: '/finance/payslip' },
+
+  // team
+  { name: 'Leave Approval', path: '/team/LeaveApproval' },
+  { name: 'Permission  Approval', path: '/team/PermissionApproval' },
+  { name: 'Attendance Report ', path: '/team/AttendanceReport' },
+  { name: 'Today Attendance ', path: '/team/TodayAttendance' },
+
 ];
 
 // Styled Components
